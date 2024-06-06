9d252a169a880bf134b975ee25b38b6233bfcb7d
Owen <Owen@Mes-Computer.local>
2024-06-06 17:57:42 +0100

    initial commit

---
+++ next.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'cdn.imagin.studio',
+          port: '',
+        },
+      ],
+    },
+  }
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         remotePatterns: ["cdn.imagin.studio"]
+//     }
+// }
+
+// module.exports = nextConfig
